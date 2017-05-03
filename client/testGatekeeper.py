f9fc18eb95bc5a38f3d6f7244cb23a0207752a56
C-o-r-E <can.of.tuna@gmail.com>
2017-05-03 00:16:12 -0400

    adding more tests

--- client/testGatekeeper.py
+++ client/testGatekeeper.py
@@ -3,6 +3,8 @@ from gatekeeper import Gatekeeper
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from multiprocessing import Process, Lock
 from time import sleep
+import json
+import os
 
 class dummyserver(BaseHTTPRequestHandler):
 
@@ -38,7 +40,7 @@ class TestMissingServer(unittest.TestCase):
 
     def testAuth(self):
         g = Gatekeeper("http://127.0.0.2:4125")
-        ret = g.authenticate("0")
+        ret = g.authenticate("beefcafe")
         self.assertFalse(ret)
 
 class TestAuthGood(unittest.TestCase):
@@ -51,7 +53,7 @@ class TestAuthGood(unittest.TestCase):
         p.start()
         sleep(1)
         g = Gatekeeper("http://127.0.0.1:4125")
-        ret = g.authenticate("0")
+        ret = g.authenticate("beefcafe")
         self.assertTrue(ret)
 
 class TestAuthBad(unittest.TestCase):
@@ -62,9 +64,66 @@ class TestAuthBad(unittest.TestCase):
         p.start()
         sleep(1)
         g = Gatekeeper("http://127.0.0.1:4125")
-        ret = g.authenticate("0")
+        ret = g.authenticate("beefcafe")
         self.assertFalse(ret)
 
+class TestCache(unittest.TestCase):
+
+    pos_data = {}
+    neg_data = {}
+    
+    @classmethod
+    def setUpClass(cls):
+        days = ["mon", "tues", "wed", "thurs", "fri",
+                "sat", "sun"]
+
+        for d in days:
+           cls.pos_data[d] = {"start": "00:00:00", "end":"23:59:59"}
+           cls.neg_data[d] = {"start": "04:00:00", "end":"04:00:01"}
+
+        #now create the db test files
+        base = os.path.dirname(os.path.abspath(__file__))
+        db_good = "{}/db/{}.json".format(base, "good.test")
+        db_bad = "{}/db/{}.json".format(base, "bad.test")
+
+        try:
+            db_file = open(db_good, 'w')
+            #db_file.write(json.dumps(cls.pos_data))
+            json.dump(cls.pos_data, db_file)
+            db_file.close()
+
+            db_file = open(db_bad, 'w')
+            #db_file.write(json.dumps(cls.neg_data))
+            json.dump(cls.pos_data, db_file)
+            db_file.close()
+        except Exception as err:
+            print("Error writing test DB files")
+            print("err: [{}]".format(err))
+            raise
+
+    def test_json_pos(self):
+        g = Gatekeeper("http://127.0.0.1:4125")
+        ret = g.json_has_access_now(json.dumps(TestCache.pos_data))
+        self.assertTrue(ret)
+
+    def test_json_neg(self):
+        g = Gatekeeper("http://127.0.0.1:4125")
+        ret = g.json_has_access_now(json.dumps(TestCache.neg_data))
+        self.assertFalse(ret)
+
+    def test_cache_positive(self):
+        g = Gatekeeper("http://127.0.0.1:4125")
+        ret = g.auth_from_cache("good.test")
+        self.assertTrue(ret)
+
+    def test_cache_negative(self):
+        g = Gatekeeper("http://127.0.0.1:4125")
+        ret = g.auth_from_cache("bad.test")
+        self.assertFalse(ret)
+        
+
+# REMOVE THIS
+"""
 class TestCoreyAuth(unittest.TestCase):
 
     def testCorey(self):
@@ -75,6 +134,7 @@ class TestCoreyAuth(unittest.TestCase):
         g.update_cache(uid)
         cached = g.auth_from_cache(uid)
         self.assertTrue(cached)
+"""
 
 if __name__ == '__main__':
     unittest.main()
