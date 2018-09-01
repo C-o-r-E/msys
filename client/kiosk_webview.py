7ec5e70d8ca32662b8e9c19d6d704923d4b92bc9
C-o-r-E <can.of.tuna@gmail.com>
2018-09-01 11:13:17 -0400

    fix url generation

--- client/kiosk_webview.py
+++ client/kiosk_webview.py
@@ -6,20 +6,26 @@ from PyQt5.QtWebKitWidgets import QWebView
 from PyQt5.QtCore import QUrl, pyqtSlot
 
 class MainWindow(QMainWindow):
+
+	baseUrl = "https://msys.heliosmakerspace.ca/members"
+
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
 
         self.browser = QWebView()
-        self.browser.setUrl(QUrl("https://msys.heliosmakerspace.ca"))
+        self.browser.setUrl(QUrl(baseUrl))
 
         self.setCentralWidget(self.browser)
 
         #self.setWindowState(Qt.WindowMaximized)
         self.show()
 
+	def set_base_url(newUrl: str) -> None:
+		self.baseUrl = newUrl
+
     @pyqtSlot(str)
-    def navigate_to_url(self, url):
-        print("navigate_to_url: [{}]".format(url))
+    def navigate_to_url(self, url): #todo: fix naming (url is an id right now) ... after the demo
+        print("navigate_to_url: [{}/cards/checkin/{}/]".format(self.baseUrl, url))
         q = QUrl(url)
         if q.scheme() == "":
             q.setScheme("http")
